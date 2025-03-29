3b4c0688c0c96c442214e53d31af97a41649aae5
SALMONLAD <leedonghyeok2008@gmail.com>
2025-03-29 14:17:58 +0900

    Sum to MUL

--- STARLAB_ps1.c
+++ STARLAB_ps1.c
@@ -4,7 +4,7 @@ int main() {
     int a, b, mul;
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
-    mul = a * b;
+    mul = a + b;
     printf("Multiple: %d\n", mul);
     return 0;
 }
